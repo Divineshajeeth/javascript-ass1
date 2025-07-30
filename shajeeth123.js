2393d8f5b52d60cc996aa914b69cdbd5411caf9e
shajeeth <shajeethshajeeth5@gmail.com>
2025-07-30 11:38:26 +0530

    comitt

--- shajeeth123.js
+++ shajeeth123.js
@@ -6,25 +6,31 @@ for (let i = 0; i < number; i++) {
     })
 
 }
+  var song = null
+
 document.addEventListener("keydown", function (event) {
     console.log(event.key);
     playsound(event.key);
 })
-        
+
 
 
 function playsound(key) {
+    if (song){
+        song.pause()
+        song.currentTime = 0
+    }
     switch (key) {
-        case "A❤️": const song1 = new Audio('song/Ek Ka Nam Sinu Ringtone Download - MobCup.Com.Co.mp3');
-            song1.play();
+        case "A❤️": song = new Audio('song/Ek Ka Nam Sinu Ringtone Download - MobCup.Com.Co.mp3');
+            song.play();
             break;
 
-        case "B❤️": const song2 = new Audio('song/minnal-wala-song-ringtone-download-mobcup-com-co-67236 (1).mp3');
-            song2.play();
+        case "B❤️": song = new Audio('song/minnal-wala-song-ringtone-download-mobcup-com-co-67236 (1).mp3');
+            song.play();
             break;
 
-        case "C❤️": const song3 = new Audio('song/nadukaatildhanimaiyuvan-12652.mp3');
-            song3.play();
+        case "C❤️":  song = new Audio('song/nadukaatildhanimaiyuvan-12652.mp3');
+            song.play();
             break;
 
 
